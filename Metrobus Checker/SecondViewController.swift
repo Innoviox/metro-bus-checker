6125d9543c86e85b6a20f485485164855cc6f0e3
Innoviox <sarras305@gmail.com>
2017-12-08 18:30:18 -0500

    Initial Commit

---
+++ Metrobus Checker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Metrobus Checker
+//
+//  Created by Sarah Leavitt on 12/8/17.
+//  Copyright © 2017 Innoviox. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

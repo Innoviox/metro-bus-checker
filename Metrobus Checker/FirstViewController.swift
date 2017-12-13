4bef55f6818918bed7a77cb5b7416d3d74e4e85b
Innoviox <sarras305@gmail.com>
2017-12-13 15:10:09 -0500

    added a map

--- Metrobus Checker/FirstViewController.swift
+++ Metrobus Checker/FirstViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class FirstViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
